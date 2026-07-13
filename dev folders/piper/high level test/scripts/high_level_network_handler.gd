ff929fb44817195ffba315057f5f472385301db9
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2026-07-13 11:29:05 -0600

    phantom cameras and ide addon

--- dev folders/piper/high level test/scripts/high_level_network_handler.gd
+++ dev folders/piper/high level test/scripts/high_level_network_handler.gd
@@ -6,11 +6,13 @@ const PORT : int = 50000
 var peer : ENetMultiplayerPeer
 
 func start_server() -> void:
+	print("server started")
 	peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer
 
 func start_client() -> void:
+	print("client started")
 	peer = ENetMultiplayerPeer.new()
 	peer.create_client(IP_ADDRESS, PORT)
 	multiplayer.multiplayer_peer = peer
