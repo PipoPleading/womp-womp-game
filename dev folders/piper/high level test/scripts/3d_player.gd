d5f7d66d4df08204b9dfcd44448c7467bb52d0c7
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2026-07-15 11:51:58 -0600

    partial 3d multiplayer

--- dev folders/piper/high level test/scripts/3d_player.gd
+++ dev folders/piper/high level test/scripts/3d_player.gd
@@ -1,5 +1,13 @@
 extends CharacterBody3D
 
+## everything needs to be loosely coupled
+# momentum based movement would be really nice, so having active accelarations instead of flat impulses
+# players are going to need to check for fall distance
+# players are going to need misc timers for things like stepped_yolk or dripping_yolk seperately
+# no sound when crouched, louder sound when sprinting, medium sound when walking
+# equipped items and a default off hand no matter what will be relevant
+# players should have a death and alive state to differentiate from spectating and playing or menuing
+
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -11,12 +19,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("m_left", "m_right", "m_up", "m_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
--- dev folders/piper/high level test/scripts/high_level_network_handler.gd
+++ dev folders/piper/high level test/scripts/high_level_network_handler.gd
@@ -5,6 +5,11 @@ const PORT : int = 50000
 
 var peer : ENetMultiplayerPeer
 
+##outline of what needs to be done for better multiplayer handling
+# have export of game and see if netplay is possible
+# from there, iron out bugs like someone being a relative server/client
+# IP_parsing is probably gonna be needed, also unique id per instance
+
 func start_server() -> void:
 	print("server started")
 	peer = ENetMultiplayerPeer.new()
