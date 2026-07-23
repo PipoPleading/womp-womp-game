766d2c9aa5c4478b229a80313da3a71f9439781e
crazyman222214 <schadejr617@gmail.com>
2026-07-22 22:42:31 -0600

    Weapon Basic Attack (WIP)

--- dev folders/chris/Resources/weapon_data.gd
+++ dev folders/chris/Resources/weapon_data.gd
@@ -1,12 +1,12 @@
 extends Resource
 class_name WeaponData
 
-enum WeaponType {Melee, Projectile, Consumable}
+enum WeaponType {Melee, Projectile}
 
 @export var name : String
 @export var type : WeaponType
 @export var damage : int
-@export var mesh : Mesh
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- dev folders/chris/Scripts/player.gd
+++ dev folders/chris/Scripts/player.gd
@@ -1,17 +1,35 @@
 extends CharacterBody3D
 
-var weapon : Weapon
 const SPEED : float = 5.0
 const JUMP_VELOCITY : float = 4.5
+
+
+const TEST_WEAPON = preload("res://dev folders/chris/Weapons/frying_pan.tscn")
+var weapon_instance
+var t : float = 0;
+
+
+var attack_rotation : Vector3
+var is_attacking = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+	if is_attacking and t < 2:
+		attack_lerp()
+	elif is_attacking:
+		is_attacking = false
+		t = 0
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_pressed("ui_attack"):
+		is_attacking = true
+		attack_rotation = weapon_instance.rotation_degrees + Vector3(90, 0, 0)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,9 +44,20 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-
-#func SetWeapon(weapon : WeaponData):
-	#weapon_data = weapon
-
-#func _init() -> void:
+func equip_weapon() -> void:
+	weapon_instance = TEST_WEAPON.instantiate()
+	add_child(weapon_instance)
+	weapon_instance.position = Vector3(1,0,0)
+	weapon_instance.rotation_degrees = Vector3(0, 0, 180)
 	
+
+func _ready() -> void:
+	equip_weapon()
+
+func attack_lerp() -> void:
+	t += 0.1
+	weapon_instance.rotation_degrees = lerp(weapon_instance.rotation_degrees, attack_rotation, t * 0.1)
+
+	#TODO: LEARN TWEEN
+	#Im tweening it
+		
