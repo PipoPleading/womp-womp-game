2abf70b78136af0ce83b9e5518dd6e4930e2b4bc
crazyman222214 <schadejr617@gmail.com>
2026-07-23 01:41:19 -0600

    Finished Frying Pan

--- dev folders/chris/Scripts/player.gd
+++ dev folders/chris/Scripts/player.gd
@@ -5,9 +5,8 @@ const JUMP_VELOCITY : float = 4.5
 
 
 const TEST_WEAPON = preload("res://dev folders/chris/Weapons/frying_pan.tscn")
-var weapon_instance
-var t : float = 0;
-
+var weapon_instance : Node3D
+var t : float = 0
 
 var attack_rotation : Vector3
 var is_attacking = false
@@ -16,20 +15,13 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if is_attacking and t < 2:
-		attack_lerp()
-	elif is_attacking:
-		is_attacking = false
-		t = 0
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if Input.is_action_pressed("ui_attack"):
-		is_attacking = true
-		attack_rotation = weapon_instance.rotation_degrees + Vector3(90, 0, 0)
+	if Input.is_action_just_pressed("ui_attack"):
+		weapon_instance.get_node("Weapon").attack()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -47,17 +39,12 @@ func _physics_process(delta: float) -> void:
 func equip_weapon() -> void:
 	weapon_instance = TEST_WEAPON.instantiate()
 	add_child(weapon_instance)
-	weapon_instance.position = Vector3(1,0,0)
-	weapon_instance.rotation_degrees = Vector3(0, 0, 180)
+	weapon_instance.position = Vector3(1,0.5,0)
 	
 
 func _ready() -> void:
 	equip_weapon()
 
-func attack_lerp() -> void:
+func attack_lerp(lerp_speed: float) -> void:
 	t += 0.1
-	weapon_instance.rotation_degrees = lerp(weapon_instance.rotation_degrees, attack_rotation, t * 0.1)
-
-	#TODO: LEARN TWEEN
-	#Im tweening it
-		
+	weapon_instance.rotation_degrees = lerp(weapon_instance.rotation_degrees, attack_rotation, t * lerp_speed)
--- dev folders/chris/Scripts/weapon.gd
+++ dev folders/chris/Scripts/weapon.gd
@@ -2,7 +2,8 @@ extends Node
 class_name Weapon
 
 @export var weapon_data : WeaponData
-
+@export var pivot_point : Node3D
+var is_attacking : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,3 +11,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func attack() -> void:
+	if is_attacking:
+		return
+	else:
+		is_attacking = true
+	
+	var tween = create_tween()
+	tween.tween_property(pivot_point, "rotation_degrees", Vector3(90, -45, 0), 0.5).as_relative()
+	tween.tween_property(pivot_point, "rotation_degrees", Vector3(-90, 45, 0), 2).as_relative()
+	
+	tween.finished.connect(func(): is_attacking = false)
