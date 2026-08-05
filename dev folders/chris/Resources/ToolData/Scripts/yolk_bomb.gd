27a2d24f0be283372c42dcd6055bd1aa30ccebe5
crazyman222214 <schadejr617@gmail.com>
2026-08-05 01:24:20 -0600

    Starting On Yolk Bomb

--- dev folders/chris/Resources/ToolData/Scripts/yolk_bomb.gd
+++ dev folders/chris/Resources/ToolData/Scripts/yolk_bomb.gd
@@ -1,14 +1,14 @@
-@abstract
-extends WeaponData
-class_name ToolData
-
+extends ToolData
+class_name YolkBomb
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-@abstract func attack(pivot_point: Node3D, tween: Tween) -> void
+func attack(pivot_point: Node3D, tween: Tween) -> void:
+	pass
--- dev folders/chris/Scripts/yolk_bomb_deployable.gd
+++ dev folders/chris/Scripts/yolk_bomb_deployable.gd
@@ -1,14 +1,12 @@
-@abstract
-extends WeaponData
-class_name ToolData
+extends Node
 
+@export var explosion_time : float = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-@abstract func attack(pivot_point: Node3D, tween: Tween) -> void
