bc5c2eebf41e5d05588eed5d82a3d1c8941b64c5
crazyman222214 <schadejr617@gmail.com>
2026-08-10 20:31:41 -0600

    Adding Signals

--- game_manager.gd
+++ game_manager.gd
@@ -1,14 +1,27 @@
 extends Node
 
-var timer : Timer
-@export var game_start_time : float
+#
+var round_timer : Timer
+
+#Making it clear how long in minutes a round lasts
+var game_time : float = 60 * 5
+
+#This would get emitted to each player 
+#and the player will connect the positions to their local minimap
+signal update_minimap_positions(enemy_positions : Array[Vector3])
+
+#This would be for starting the game (i.e when the lobby starts)
+signal start_game()
+
+#This would be used for emitting when the game ends as well as
+#if that player was the winner
+signal finish_game(is_winning_player : bool)
 
 
+#assigning timer and starting it (realistically wouldn't start on ready)
 func _ready() -> void:
-	timer = Timer.new()
-	add_child(timer)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	round_timer = Timer.new()
+	add_child(round_timer)
+	
+	round_timer.wait_time = game_time
+	round_timer.start()
